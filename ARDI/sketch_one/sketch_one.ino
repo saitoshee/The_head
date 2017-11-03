d4a89a5fb4eae123138441baea544bb9b931918d
saitoshee <saitoshee@yandex.ru>
2017-11-03 22:58:38 +0400

    update

--- ARDI/sketch_one/sketch_one.ino
+++ ARDI/sketch_one/sketch_one.ino
@@ -1,13 +1,19 @@
+//scetch_one||Ar
+
+int ledPin = 13;
+int delayTime = 5;
 void setup() {
   // put your setup code here, to run once:
-  pinMode(13,OUTPUT);
+  pinMode(ledPin,OUTPUT);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(13,HIGH);
-  delay(500);
-  digitalWrite(13,LOW);
-  delay(500);
-  }
+  digitalWrite(ledPin,HIGH);
+  delay(delayTime);
+  digitalWrite(ledPin,LOW);
+  delay(delayTime);
+ // delayTime = delayTime + 1000;
+  
+}
---
+++ ARDI/sketch_one_1/sketch_one_1.ino
@@ -0,0 +1,14 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  int a = 2;
+  int b = 4;
+  int c = a + b;
+  Serial.println(c);
+}
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
