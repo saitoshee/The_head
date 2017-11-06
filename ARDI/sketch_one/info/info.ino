f8ecc623b6fd29ec562eeeea0f3854f79f26b4f5
saitoshee <saitoshee@yandex.ru>
2017-11-06 20:09:44 +0400

    l

---
+++ ARDI/sketch_one/info/info.ino
@@ -0,0 +1,42 @@
+/*
+ *                            AVR                   ESP
+ *      
+ * short int            -32768..32767            -32768..32767
+ * unsigned short int    0..65535                 0..65535
+ * int                  -32768..32767            -2147483648..2147483647
+ * unsigned int         0..65535                 0..4294967295
+ * long int             -2147483648..2147483647  -2147483648..2147483647
+ * unsigned long int    0..4294967295            0..4294967295
+ * 
+ * float
+ * _________________________________________________________
+ * char                 int8_t      -128..127     1b = 8byte
+ * unsigned char        uint8_t     0.255
+ * int                  int16_t     -32768..32767 2b = 16byte
+ * unsigned int         uint16_t    0..65535        
+ * long int             int32_t     -2^31..2^31-1
+ * unsigned long int    uint32_t    0..2^31-1
+ * 
+ * constants:
+ * #define NAME  1
+ * const int8_t ledPin = 13;
+ * 
+ * functions:
+ * 
+ * int sqr(int a)
+ * {
+ *    return a*a;
+ * }
+ * 
+*/
+
+void setup() {
+  int16_t = a,b,c;
+  
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
