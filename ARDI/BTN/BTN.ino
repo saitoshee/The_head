11ae6ffa140effdfea2f180301132674c98160ce
saitoshee <saitoshee@yandex.ru>
2017-11-07 17:52:13 +0400

    btn

---
+++ ARDI/BTN/BTN.ino
@@ -0,0 +1,14 @@
+const int16_t BTN = 2;   
+const int16_t LED =  13;     
+
+void setup() {
+
+  pinMode(LED, OUTPUT);
+  pinMode(BTN, INPUT);
+}
+
+void loop() {
+
+    digitalWrite(LED, ! digitalRead(BTN));
+
+}
