49ec0584f47f09a5fd66c3a0d8688969da8626fb
saitoshee <saitoshee@yandex.ru>
2017-11-20 19:43:25 +0400

    d

---
+++ Sharp/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿// Ctr+K+D
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   /*
+            int a = 2;
+            int b = 3;
+            int c = 4;
+            
+              int c = b / a; // 1
+               double c = (double)b / a;  // 1,5
+               int c = 1 / 2; // 0
+               double angle = 3.14159;
+
+               Console.WriteLine(Math.Sin(angle));
+            
+            var c = a * b; // компилятор самостоятельно определяет тип с
+
+            var d = c.ToString(); 
+            
+             //Console.WriteLine(b = c); // = это такая же операция как + -
+            a = b = c;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+                  var e = 2;
+
+            Console.WriteLine(e++ + ++e);
+            */
+            var a = 5.0;
+            var b = a + 0.5;
+            Console.WriteLine(b);
+
+
+            Console.ReadKey();
+        }
+    }
+}
