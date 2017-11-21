c6c5a756c61e8bd8c1b6fe14b29178309c996a4b
saitoshee <saitoshee@yandex.ru>
2017-11-21 21:34:16 +0400

    g

---
+++ Sharp/ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        private static string PrintGreeting(string name, double salary)
+        {
+            string a = Console.Write(" ");
+
+
+
+            // возвращает "Hello, <name>, your salary is <salary>"
+            return a; 
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
