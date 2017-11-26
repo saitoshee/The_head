16421db45eb185f65ec875e795f5ab36e538b966
saitoshee <saitoshee@yandex.ru>
2017-11-26 22:39:34 +0400

    g

---
+++ Sharp/f105cca4-2fe6-4842-9894-028c9c6b10b50_Pluralize.exercise/PluralizeTask.cs
@@ -0,0 +1,12 @@
+﻿namespace Pluralize
+{
+	public static class PluralizeTask
+	{
+		public static string PluralizeRubles(int count)
+		{
+			// Напишите функцию склонения слова "рублей" в зависимости от предшествующего числительного count.
+
+			return "руб.";
+		}
+	}
+}
